f96e69fc2d237a6fd238cd6987f5ad7147a37ea8
Ro <53170106+dtkieXRo@users.noreply.github.com>
2025-05-30 12:10:59 +0700

    Create config.js

---
+++ assets/css/config.js
@@ -0,0 +1,13 @@
+const CONFIG = {
+    GITHUB: {
+        USERNAME: 'dtkieXRo',
+        REPO: 'kietxlq',
+        TOKEN: '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' // Will be set via GitHub Secrets
+    },
+    API: {
+        BASE_URL: 'https://api.github.com',
+        CONTENT_URL: 'https://raw.githubusercontent.com'
+    }
+};
+
+export default CONFIG;
